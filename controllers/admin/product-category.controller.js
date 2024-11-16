66262a6a6e551eeb1bd6c56c18d29e0c2f4a1670
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-11-16 22:57:47 +0700

    hoàn thiện danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -71,3 +71,112 @@ module.exports.createPost = async (req, res) => {
     req.flash("success", "Thêm thành công danh mục sản phẩm")
     res.redirect(`${systemConfig.prefixAdmin}/product-category`)
 }
+
+// [DELETE] /admin/product-category/delete/:id
+module.exports.deleteProductCategory = async (req, res) => {
+    console.log(req.params)
+    await ProductCategory.updateOne({
+        _id: req.params.id
+    }, {
+        deleted: true,
+        deletedBy: {
+            account_id: res.locals.user.id,
+            deletedAt: Date.now()
+        }
+    })
+    req.flash("success", "Đã xóa thành công danh mục sản phẩm")
+    res.redirect("back")
+}
+
+// [PATCH] /admin/product-category/change-status/:status/:id
+module.exports.changeStatus = async (req, res) => {
+    const status = req.params.status
+    const id = req.params.id
+
+    await ProductCategory.updateOne({
+        _id: id
+    }, {
+        status: status
+    })
+    req.flash("success", "Cập nhật trạng thái thành công!")
+    res.redirect("back")
+}
+
+// [GET] /admin/product-category/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id
+        const record = await ProductCategory.findOne({
+            _id: id
+        })
+        let find = {
+            deleted: false
+        }
+
+        const records = await ProductCategory.find(find)
+        const treeRecords = createTreeHelper.tree(records);
+        // const treeRecords = createTreeHelper.tree(record)
+        res.render("admin/pages/product-category/edit.pug", {
+            pageTitle: "Chỉnh sửa danh mục sản phẩm",
+            record: record,
+            records: treeRecords
+        })
+    } catch {
+        req.flash("error", "Đường dẫn không tồn tại!")
+        res.redirect(`${systemConfig.prefixAdmin}/product-category`)
+    }
+}
+
+// [PATCH] /admin/product-category/edit:id
+module.exports.editPatch = async (req, res) => {
+    // console.log(req.body)
+
+    req.body.position = parseInt(req.body.position)
+    try {
+        const updatedBy = {
+            account_id: res.locals.user.id,
+            updatedAt: new Date()
+        }
+        console.log(updatedBy)
+        await ProductCategory.updateOne({
+            _id: req.params.id
+        }, {
+            $set: req.body,
+            $push: {
+                updatedBy: updatedBy
+            }
+        })
+        req.flash("success", "Chỉnh sửa thành công danh mục sản phẩm")
+        // res.redirect("back")
+        res.redirect(`${systemConfig.prefixAdmin}/product-category`)
+    } catch (error) {
+        // res.redirect("back")
+        req.flash("error", "Chỉnh sửa thất bại sản phẩm")
+        res.redirect(`${systemConfig.prefixAdmin}/product-category`)
+    }  
+}
+
+// [GET] /admin/product-category/detail:id
+module.exports.detail = async (req, res) => {
+    try {
+        const id = req.params.id
+        const record = await ProductCategory.findOne({
+            _id: id
+        })
+        let parentCategory = null;
+        if (record.parent_id) {
+            parentCategory = await ProductCategory.findOne({
+                _id: record.parent_id
+            })
+        }
+        // console.log(record)
+        res.render("admin/pages/product-category/detail.pug", {
+            pageTitle: record.title,
+            category: record,
+            parentCategory: parentCategory
+        })
+    } catch (error) {
+        req.flash("error", "Đường dẫn không tồn tại !")
+        res.redirect(`${systemConfig.prefixAdmin}/product-category`)
+    }
+}
--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -12,4 +12,14 @@ router.get("/create", controller.create)
 
 router.post('/create', upload.single('thumbnail'), uploadCloud.upload, controller.createPost)
 
+router.delete("/delete/:id", controller.deleteProductCategory)
+
+router.patch('/change-status/:status/:id', controller.changeStatus)
+
+router.get("/edit/:id", controller.edit)
+
+router.patch("/edit/:id", upload.single('thumbnail'), uploadCloud.upload, controller.editPatch)
+
+router.get("/detail/:id", controller.detail)
+
 module.exports = router
