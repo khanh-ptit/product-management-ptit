53a4539a59ffa11fec8204b89977d572609eea6b
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2025-03-26 16:47:24 +0700

    Update model account

--- models/account.model.js
+++ models/account.model.js
@@ -1,37 +1,41 @@
-const mongoose = require("mongoose")
-const generate = require("../helpers/generate")
+const mongoose = require("mongoose");
+const generate = require("../helpers/generate");
 
-const accountSchema = new mongoose.Schema({
+const accountSchema = new mongoose.Schema(
+  {
     fullName: String,
     email: String,
     password: String,
     token: {
-        type: String,
-        default: () => generate.generateRandomString(30)
+      type: String,
+      default: () => generate.generateRandomString(30),
     },
     phone: String,
     avatar: {
-        type: String,
-        default: "https://cellphones.com.vn/sforum/wp-content/uploads/2023/10/avatar-trang-4.jpg"
+      type: String,
+      default:
+        "https://cellphones.com.vn/sforum/wp-content/uploads/2023/10/avatar-trang-4.jpg",
     },
     address: String,
     deleted: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     role_id: String,
     status: {
-        type: String,
-        default: "active"
+      type: String,
+      default: "active",
     },
     profit: {
-        type: Number,
-        default: 0
-    }
-}, {
-    timestamps: true
-})
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Account = mongoose.model("Account", accountSchema, "accounts")
+const Account = mongoose.model("Account", accountSchema, "accounts");
 
-module.exports = Account
+module.exports = Account;
