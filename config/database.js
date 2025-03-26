ce4b806b594504a6455064af00d8558eda30beac
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2025-03-26 11:33:37 +0700

    Update README with comprehensive project documentation and add demo screenshot

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 module.exports.connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("Successfully connected")
-    } catch (error) {
-        console.log("Connect error")
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Successfully connected");
+  } catch (error) {
+    console.log("Error when connect to the DB");
+  }
+};
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const systemConfig = require("./config/system")
 const app = express()
 const port = process.env.PORT
 
+
 app.set("views", `${__dirname}/views`)
 app.set("view engine", "pug");
 app.use(express.static(`${__dirname}/public`))
